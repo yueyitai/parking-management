d2272b8351e6cc940d81538fd81af0e02c97165b
uther <517216009@qq.com>
2024-08-07 18:33:13 +0800

    完成了部分控制器

---
+++ server/db/controller/VIPInformationController.cpp
@@ -0,0 +1,49 @@
+#include "VIPInformationController.h"
+
+
+VIPInformationController::VIPInformationController(VIPInformationDAO* dao):dao(dao)
+{
+
+}
+
+VIPInformationController::~VIPInformationController()
+{
+
+}
+
+
+//登录
+std::string VIPInformationController::VIPLogin(cJSON data){
+    cJSON* root;
+    try
+    {
+        cJSON* licencePlateJSON = cJSON_GetObjectItem(&data, "licence_plate");
+        if(licencePlateJSON == NULL){
+            throw std::runtime_error("JSON解析失败！");  
+            return nullptr;
+        }
+        VIPInformation vip(dao->searchwithLicence(licencePlateJSON->string));
+        if(vip.getId() == -1){
+            return nullptr;
+        }
+        cJSON_AddStringToObject(root, "owner_name", vip.getOwnerName().c_str());
+        cJSON_AddStringToObject(root, "owner_telephone", vip.getOwnerTelephone().c_str());
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return nullptr;
+    }
+    std::string ret = cJSON_Print(root);
+    return ret;
+}
+
+//注册
+std::string VIPInformationController::VIPRegister(cJSON data){
+    
+}
+
+//充值
+std::string VIPInformationController::VIPCharge(cJSON data){
+
+}
---
+++ server/db/controller/VIPInformationController.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "../model/VIPInformationDAO.h"
+#include "cJSON.h"
+
+#include <string>
+#include <memory>
+#include <stdexcept>
+#include <iostream>
+
+class VIPInformationController
+{
+private:
+    VIPInformationDAO* dao;
+public:
+    typedef std::shared_ptr<VIPInformationController> ptr;
+    VIPInformationController(VIPInformationDAO* dao);
+    ~VIPInformationController();
+
+    //登录
+    std::string VIPLogin(cJSON data);
+
+    //注册
+    std::string VIPRegister(cJSON data);
+
+    //充值
+    std::string VIPCharge(cJSON data);
+};
+
